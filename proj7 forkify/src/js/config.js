a315c42b9a873d2a3bd2a93d85b111bd96916c05
ardit <zhaozhifei77@gmail.com>
2025-09-01 04:32:16 +1000

    resolve error of forkify

---
+++ proj7 forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "a82a316e-9cd5-492c-8ad9-3f81e73f097c";
+export const MODAL_CLOSE_SEC = 2.5;
